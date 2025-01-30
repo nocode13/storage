caacd70b6c9d5c9e65c4ed6e2f765ce5ae2800d2
khikmatjon <azizovxikmatjon04@gmail.com>
2025-01-30 18:13:15 +0500

    save

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,18 +1,7 @@
 import axios from 'axios';
 import { API_URL } from '../config/api';
-import { authStorageService } from '../lib/auth-storage';
 
 export const baseInstance = axios.create({
   baseURL: API_URL,
+  withCredentials: true,
 });
-
-baseInstance.interceptors.request.use(
-  async (config) => {
-    const token = authStorageService.getAccessToken();
-    if (token) {
-      config.headers.Authorization = token;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
